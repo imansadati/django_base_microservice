3768dfa21db5717b68a192880c33740ae45cfb56
iman sadati <iman.3adati@gmail.com>
2025-02-02 20:22:53 +0330

    refactor: Add condition for static and media rendering in settings.py

--- services/service_2/core/urls.py
+++ services/service_2/core/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from core import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls), # add this when you're in development mode
     # path('service_2/admin/', admin.site.urls), # add this when you're in production mode
 ]
 
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
