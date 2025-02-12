396dadfba973ffb7b5a347c93ed60319c9ea1f82
iman sadati <iman.3adati@gmail.com>
2025-02-12 22:52:29 +0330

    fix: Fix Permission Denied Error in Django Services

--- services/service_1/core/settings.py
+++ services/service_1/core/settings.py
@@ -103,8 +103,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 
-STATIC_ROOT = BASE_DIR / 'static'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = '/app/static'
+MEDIA_ROOT = '/app/media'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
